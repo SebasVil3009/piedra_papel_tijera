b96dd65e95eea9567785c36b4b651ded8c65631a
SebasVil3009 <94868216+SebasVil3009@users.noreply.github.com>
2022-02-22 17:08:27 -0300

    Update pi_pa_ti.py
    cambie todo el codigo por esto

--- pi_pa_ti.py
+++ pi_pa_ti.py
@@ -1,45 +1,45 @@
 import random
-selecciones = ("piedra","papel","tijera") 
+
 print("Bienvenido a Piedra, papel o tijera")
 input("Presiona ENTER para jugar")
-computadora = random.randint(1,3)
-jugador = int(input(f"selecciona 1 para {selecciones[0]} \nseleccione 2 para {selecciones[1]} \nseleccione 3 para {selecciones[2]}\n"))
-contador_j = 0
-contador_c = 0
-while contador_j <= 3 or contador_c <= 3:
-	if jugador == 1 and computadora == 1:
-		print("Hemos empatado.\nJuguemos de nuevo")
-		break
-	elif jugador == 2 and computadora == 2:
-		print("Hemos empatado.\nJuguemos de nuevo")
-		break
-	elif jugador == 3 and computadora == 3:
-		print("Hemos empatado.\nJuguemos de nuevo")
-		break
-	elif jugador == 1 and computadora == 3:
-		print("Has ganado")
-		contador_j = contador_j + 1
-		break
-	elif jugador == 2 and computadora == 1:
-		print("Has ganado")
-		contador_j = contador_j + 1
-		break
-	elif jugador == 3 and computadora == 2:
-		print("Has ganado")
-		contador_j = contador_j + 1
-		break
-	elif jugador == 1 and computadora == 2:
-		print("Perdiste")
-		contador_c = contador_c + 1
-		break
-	elif jugador == 2 and computadora == 3:
-		print("Perdiste")
-		contador_c = contador_c + 1
-		break
-	elif jugador == 3 and computadora == 1:
-		print("Perdiste")
-		contador_c = contador_c + 1
-		break
-	else:
-		print("Elegiste una opcion incorrecta")
-input(f"Vamos \n{contador_j} jugador \n{contador_c} computadora")
+
+selecciones = ("piedra","papel","tijera") 
+def reglas(seleccion_j,seleccion_c):
+	
+	resultado = ""
+	contador_j = 0
+	contador_c = 0
+	while contador_j <= 3 or contador_c <= 3:
+		
+		if seleccion_j == seleccion_c:
+			resultado = "Hemos empatado jueguemos de nuevo"
+			return resultado
+		elif seleccion_j == 1 and seleccion_c == 3:
+			resultado = "Has ganado"
+			contador_j = contador_j + 1
+			return resultado, contador_j
+		elif seleccion_j == 2 and seleccion_c == 1:
+			resultado = "Has ganado"
+			contador_j = contador_j + 1
+			return resultado, contador_j
+		elif seleccion_j == 3 and seleccion_c == 2:
+			resultado = "Has ganado"
+			contador_j = contador_j + 1
+			return resultado, contador_j
+		elif seleccion_j == 1 and seleccion_c == 2:
+			resultado = "Has perdido"
+			contador_c = contador_c + 1
+			return resultado, contador_c
+		elif seleccion_j == 2 and seleccion_c == 3:
+			resultado = "Has perdido"
+			contador_c = contador_c + 1
+			return resultado, contador_c
+		elif seleccion_j == 3 and seleccion_c == 1:
+			resultado = "Has perdido"
+			contador_c = contador_c + 1
+			return resultado, contador_c
+		else:
+			resultado = "Has seleccionado una opcion incorrecta"
+		pass
+
+print(reglas(int(input(f"selecciona 1 para {selecciones[0]} \nseleccione 2 para {selecciones[1]} \nseleccione 3 para {selecciones[2]}\n")),random.randint(1,3)))
